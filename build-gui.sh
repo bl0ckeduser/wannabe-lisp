109b5e3c72e1a1a627c6c2c65233c62458293f2b
bl0ckeduser <bl0ckedusersoft@gmail.com>
2013-08-06 18:55:25 -0400

    GUI: about box: show build date and originating commit

--- build-gui.sh
+++ build-gui.sh
@@ -3,5 +3,8 @@ make prefix.txt
 #emcc -DJS_GUI `ls *.c | grep -v '^main\.c'` -O2 -s ASM_JS=0 --embed-file prefix.txt --shell-file gui/emcc-template.html -s ALLOW_MEMORY_GROWTH=1 -o gui/main.html -s EXPORTED_FUNCTIONS="['_handle_gui_line', '_do_setup']"
 emcc -O2 -DJS_GUI -s ASM_JS=0 `ls *.c | grep -v '^main\.c'` --embed-file prefix.txt --shell-file gui/emcc-template.html -s ALLOW_MEMORY_GROWTH=1 -o gui/main.html -s EXPORTED_FUNCTIONS="['_handle_gui_line', '_do_setup', '_jsgui_error_handler']"
 
+COMMIT="$(git log | head -1 | sed 's/commit //g')"
+DATE="$(date)"
+cat gui/about.html | sed "s/%%BUILD-DATE%%/$DATE/g" | sed "s/%%BUILD-COMMIT%%/$COMMIT/g" >gui/final/code/chrome/content/about.html
 cp gui/main.html gui/final/code/chrome/content/
 cp gui/frame.html gui/final/code/chrome/content/index.html
